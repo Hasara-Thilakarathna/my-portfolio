c99864abacf872747f209662edb9cb07c961deb0
Hasara-Thilakarathna <hasarathilakarathnaa4@gmail.com>
2023-12-29 10:56:34 +0530

    Update home component

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -2,14 +2,21 @@ import './Header.css';
 
 export function Header() {
     return (
-        <header className="flex justify-around p-2 bg-slate-900 text-white items-center h-24">
-            <div className="text-3xl">Hasara Thilakarathna <sub className="text-xs">Portfolio</sub></div>
-            <nav id="nav-bar" className="list-none flex justify-center gap-14">
-                <li>Home</li>
-                <li>About me</li>
-                <li>Projects</li>
-                <li>Contact me</li>
-            </nav>
-        </header>
+        <div className="bg-slate-900">
+            <header className=" flex justify-around pt-10  text-white items-center h-24">
+                <div id="title" className="text-5xl">Hasara Thilakarathna <sub className="text-xs">Portfolio</sub></div>
+
+                <nav id="nav-bar" className="list-none flex justify-center gap-14">
+                    <li>Home</li>
+                    <li>About me</li>
+                    <li>Projects</li>
+                    <li>Contact me</li>
+                </nav>
+            </header>
+            <div className="pt-10 mx-32">
+                <hr className=""/>
+            </div>
+        </div>
+
     );
 }
