8270915883176b01acd783ff07512b9c1bc13fb2
Hasara-Thilakarathna <hasarathilakarathnaa4@gmail.com>
2024-09-14 21:53:02 +0530

    Make build file changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // this will output the build to a `build` directory
+  },
 })
